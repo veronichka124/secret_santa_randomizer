f3399bb437ff433417afb8b7f415dce60cfbaa87
Veronika H. <veronika.hromchenkova@gmail.com>
2024-12-19 17:23:34 +0200

    Added service worker
    Added service worker script.js for Offline Capability

--- script.js
+++ script.js
@@ -154,3 +154,20 @@ refreshBtn.addEventListener("click", refreshNames);
 submitNamesBtn.addEventListener("click", submitNames);
 
 updateRefreshButtonVisibility();
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('secret-santa-cache').then(cache => {
+      return cache.addAll(['./', './index.html', './reindeer.png']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
